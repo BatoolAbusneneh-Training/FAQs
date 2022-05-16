74f7ca534ca13d5a7586912f56e6881dbb3d8af7
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-16 15:16:29 +0300

    import all component in app page

---
+++ faqs/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./component/navBar/NavBar";
+import Faqs from "./component/FAQs/Faqs"
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+       <Routes>
+        <Route path="/faqs" element={<Faqs />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
