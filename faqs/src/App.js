5b36d10226f142e993b5f898172699d676f2fd45
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-16 21:21:22 +0300

    clean code

--- faqs/src/App.js
+++ faqs/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import NavBar from "./component/navBar/NavBar";
-import Faqs from "./component/FAQs/Faqs"
+import Faqs from "./component/FAQs/Faqs";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavBar />
-       <Routes>
+      <Routes>
         <Route path="/faqs" element={<Faqs />} />
       </Routes>
     </div>
