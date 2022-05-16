b391699fb950897dc70470126c28c364a50388f7
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-16 21:22:02 +0300

    create navbar responsive

--- faqs/src/component/navBar/NavBar.js
+++ faqs/src/component/navBar/NavBar.js
@@ -7,25 +7,25 @@ const NavBar = () => {
     <div className="navbar">
       <img className="logo" src="./images/Twenteefore - Logo 2.png" />
       <div id="navbar">
-      <Link className="homeLink" to="/">
-        Home
-      </Link>
-      <Link className="orderLink" to="/">
-        Order
-      </Link>
-      <Link className="leaderboardsLink" to="/">
-        Leaderboards
-      </Link>
-      <Link className="marketplaceLink" to="/">
-        Marketplace
-      </Link>
-      <Link className="faqsLink" to="/faqs">
-        FAQs
-      </Link>
-   
-      <Link className="loginLink" to="/">
+        <Link className="homeLink" to="/">
+          Home
+        </Link>
+        <Link className="orderLink" to="/">
+          Order
+        </Link>
+        <Link className="leaderboardsLink" to="/">
+          Leaderboards
+        </Link>
+        <Link className="marketplaceLink" to="/">
+          Marketplace
+        </Link>
+        <Link className="faqsLink" to="/faqs">
+          FAQs
+        </Link>
+
+        <Link className="loginLink" to="/">
           <button id="loginLink">Log In</button>
-      </Link>
+        </Link>
       </div>
     </div>
   );
