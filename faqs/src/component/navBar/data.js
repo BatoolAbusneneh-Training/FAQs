4e27379bd2eed6d0231a36fca689502cc287d351
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-22 13:55:33 +0300

    create data file

---
+++ faqs/src/component/navBar/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
+];
