faf76d74ae134dd3d47db3eba9a7e84ead93743e
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-22 13:55:51 +0300

    create faqs file

--- faqs/src/App.js
+++ faqs/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import NavBar from "./component/navBar/NavBar";
-import Faqs from "./component/FAQs/Faqs";
+import Faqs from "./pages/FAQs/Faqs";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/faqs" element={<Faqs />} />
+      <Route path="/faqs" element={<Faqs />} />
+      {/* <Route path="/market" element={<Market />} />
+      <Route path="/faqs" element={<Faqs />} />
+      <Route path="/faqs" element={<Faqs />} />
+      <Route path="/faqs" element={<Faqs />} /> */}
       </Routes>
     </div>
   );
