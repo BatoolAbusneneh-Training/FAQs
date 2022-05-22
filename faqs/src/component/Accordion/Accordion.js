4d46e22456b7a45074d7c29506f75efdabcb9cde
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-22 15:53:19 +0300

    craete accordion page and design

--- faqs/src/component/Accordion/Accordion.js
+++ faqs/src/component/Accordion/Accordion.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Accordion.css"
+import "./Accordion.css";
 import {
   UncontrolledAccordion,
   AccordionItem,
@@ -9,7 +9,7 @@ import {
 import React from "react";
 const Accordion = () => {
   return (
-    <div className="qustion">
+    <div className="accordion">
       <UncontrolledAccordion defaultOpen="1">
         <AccordionItem>
           <AccordionHeader targetId="1">
@@ -21,7 +21,7 @@ const Accordion = () => {
             noting that just about any HTML can go within the .accordion-body,
             though the transition does limit overflow.
           </AccordionBody>
-        </AccordionItem>
+        </AccordionItem>{" "}
         <AccordionItem>
           <AccordionHeader targetId="2">
             <strong>Lorem hsidhs ?</strong>
@@ -35,12 +35,55 @@ const Accordion = () => {
         </AccordionItem>
         <AccordionItem>
           <AccordionHeader targetId="3">
-            <strong>Lorem hsidhs ?</strong>
+            <strong>Lorem hsidhs  fdntjknv ?</strong>
           </AccordionHeader>
           <AccordionBody accordionId="3">
             This is the third item's accordion body. You can modify any of this
             with custom CSS or overriding our default variables. It's also worth
             noting that just about any HTML can go within the .accordion-body ,
+            though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>{" "}
+        <AccordionItem>
+          <AccordionHeader targetId="4">
+            <strong>Lorem ghfj hrgru iitkrmr ?</strong>
+          </AccordionHeader>
+          <AccordionBody accordionId="4">
+            This is the first item's accordion body. You can modify any of this
+            with custom CSS or overriding our default variables. It's also worth
+            noting that just about any HTML can go within the .accordion-body,
+            though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>{" "}
+        <AccordionItem>
+          <AccordionHeader targetId="5">
+            <strong>Lorem mmkl jjdjfijjoid jjfsdjjfs ?</strong>
+          </AccordionHeader>
+          <AccordionBody accordionId="5">
+            This is the first item's accordion body. You can modify any of this
+            with custom CSS or overriding our default variables. It's also worth
+            noting that just about any HTML can go within the .accordion-body,
+            though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>{" "}
+        <AccordionItem>
+          <AccordionHeader targetId="6">
+            <strong>Lorem ffsdfdfs ffgggfgfdd ?</strong>
+          </AccordionHeader>
+          <AccordionBody accordionId="6">
+            This is the first item's accordion body. You can modify any of this
+            with custom CSS or overriding our default variables. It's also worth
+            noting that just about any HTML can go within the .accordion-body,
+            though the transition does limit overflow.
+          </AccordionBody>
+        </AccordionItem>  <AccordionItem>
+          <AccordionHeader targetId="7">
+            <strong>Lorem  msnfg heijmxb nkoaii?</strong>
+          </AccordionHeader>
+          <AccordionBody accordionId="7">
+            This is the first item's accordion body. You can modify any of this
+            with custom CSS or overriding our default variables. It's also worth
+            noting that just about any HTML can go within the .accordion-body,
             though the transition does limit overflow.
           </AccordionBody>
         </AccordionItem>
@@ -49,19 +92,3 @@ const Accordion = () => {
   );
 };
 export default Accordion;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
