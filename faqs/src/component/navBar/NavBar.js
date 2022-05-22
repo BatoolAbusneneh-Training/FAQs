065c5b9f96e25962dfd4b27d4e28c584fd4306da
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-22 15:53:51 +0300

    craete navbar

--- faqs/src/component/navBar/NavBar.js
+++ faqs/src/component/navBar/NavBar.js
@@ -1,35 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 import { data } from "./data";
 
 const NavBar = () => {
-  return (
-    <div className="navbar">
+  const [activeHam, setActiveHam] = useState(false);
+
+  const menuItems = (
+    <>
       {data.map((element, index) => {
-        return <div>
-          <Link id="li" to={element.path}>
-          {element.name}
-        </Link>
-        </div>;
-      })}    
-        <img className="logo" src="./images/Twenteefore - Logo 2.png" />
-      <div id="navbar">
-
-      {data.map((ele, i) => {
-        return <div key={i}>
-          <Link className={ele.className} to={ele.path}>
-          {ele.name}
-        </Link>
-        </div>;
+        return (
+          <Link className={element.className} to={element.path}>
+            {element.name}
+          </Link>
+        );
       })}
+    </>
+  );
 
-
-        <Link className="loginLink" to="/">
-          <button id="loginLink">Log In</button>
-        </Link>
-      </div>
+  return (
+    <div className="navbar-container">
+      <nav>
+        <div className="nav-container">
+          <img className="nav-brand" src="./images/Twenteefore - Logo 2.png" />
+          <div className="menu">{menuItems}</div>
+          <button className="logout">login</button>
+          <button
+            className={activeHam ? "hamburger active-hamburger" : "hamburger"}
+            onClick={() => setActiveHam(!activeHam)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </nav>
+      {activeHam && <div className="nav-dropdown">{menuItems}</div>}
     </div>
+
+    // <div>
+    //   <input type="checkbox" id="hamburger-input" className="burger-shower" />
+    //   <label id="hamburger-menu" for="hamburger-input">
+    //     <nav id="sidebar-menu">
+    //       {data.map((ele, i) => {
+    //         return (
+    //           <div key={i}>
+    //             <ul>
+    //               <li>
+    //                 <Link id="li" to={ele.path}>
+    //                   {ele.name}
+    //                 </Link>{" "}
+    //                 <br />
+    //               </li>
+    //             </ul>
+    //           </div>
+    //         );
+    //       })}
+    //     </nav>
+    //   </label>
+
+    //   <div class="overlay"></div>
+
+    //   <img className="logo" src="./images/Twenteefore - Logo 2.png" />
+    //   <div id="navbar">
+    //     {data.map((ele, i) => {
+    //       return (
+    //         <div key={i}>
+    //           <Link className={ele.className} to={ele.path}>
+    //             {ele.name}
+    //           </Link>
+    //         </div>
+    //       );
+    //     })}
+
+    //     <Link className="loginLink" to="/">
+    //       <button id="loginLink">Log In</button>
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
 export default NavBar;
