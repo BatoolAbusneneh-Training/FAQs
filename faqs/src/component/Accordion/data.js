8603d71e9393088a8789ae3b96050db21d2c9d48
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-23 10:57:05 +0300

    create data file to imoport accordion data

---
+++ faqs/src/component/Accordion/data.js
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    question: "Lorem ?",
+    targetId: "1",
+    accordionId: "1",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem as nff gyu ?",
+    targetId: "2",
+    accordionId: "2",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem hsdf vrtry ikm?",
+    targetId: "3",
+    accordionId: "3",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem hhkkjnvf ?",
+    targetId: "4",
+    accordionId: "4",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem hhhjh?",
+    targetId: "5",
+    accordionId: "5",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem mnkkljo?",
+    targetId: "6",
+    accordionId: "6",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+  {
+    question: "Lorem jjhjhh?",
+    targetId: "7",
+    accordionId: "7",
+    answer:
+      "This is the first item's accordion body. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow",
+  },
+];
