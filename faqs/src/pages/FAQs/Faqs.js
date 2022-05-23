c921007b120658f33039d18c94cdbe66686debe0
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-23 11:00:20 +0300

    create faqs module css

--- faqs/src/pages/FAQs/Faqs.js
+++ faqs/src/pages/FAQs/Faqs.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import "./Faqs.css";
+import faqs from "./faqs.module.css";
 import Accordion from "../../component/Accordion/Accordion";
 import "bootstrap/dist/css/bootstrap.min.css";
 const Faqs = () => {
   return (
-    <div className="faqspage">
-      <h1 className="frequently">Frequently Asked Questions</h1>
-      <h1 id="frequently">FAQs</h1>
-      <h6 className="feelFree">
+    <div className={faqs.faqspage}>
+      <h1 className={faqs.frequently}>Frequently Asked Questions</h1>
+      <h1 id={faqs.frequently}>FAQs</h1>
+      <h6 className={faqs.feelFree}>
         Feel free to send us your questions or comments below
       </h6>
-     <Accordion/>
+      <Accordion />
     </div>
   );
 };
