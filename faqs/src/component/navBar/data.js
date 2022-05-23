cbb7cb9e0c76d4616d8fd3d4c94e719ed45fa600
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-23 10:57:24 +0300

    create data file to imoport navbar data

--- faqs/src/component/navBar/data.js
+++ faqs/src/component/navBar/data.js
@@ -1,7 +1,11 @@
 export const data = [
-   { name: "Home", path: "/home", className: "homeLink" },
- { name: "FAQs", path: "/faqs", className: "faqsLink" },
+  { name: "Home", path: "/home", className: "homeLink" },
+  { name: "FAQs", path: "/faqs", className: "faqsLink" },
   { name: "Order", path: "/order", className: "orderLink" },
-  { name: "Leaderboards", path: "/leaderboards", className: "leaderboardsLink" },
+  {
+    name: "Leaderboards",
+    path: "/leaderboards",
+    className: "leaderboardsLink",
+  },
   { name: "Marketplace", path: "/marketplace", className: "marketplaceLink" },
 ];
