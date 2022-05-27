5f6e1ab893a3c478adb569e35ff2a3d8d6af17e8
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-05-27 23:02:18 +0300

    create route login and register

--- faqs/Frontend/src/App.js
+++ faqs/Frontend/src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import NavBar from "./component/navBar/NavBar";
 import Faqs from "./pages/FAQs/Faqs";
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginPage/LoginPage";
+import UsersPage from "./pages/usersPage/UsersPage";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
       <Route path="/faqs" element={<Faqs />} />
-      {/* <Route path="/market" element={<Market />} />
-      <Route path="/faqs" element={<Faqs />} />
-      <Route path="/faqs" element={<Faqs />} />
-      <Route path="/faqs" element={<Faqs />} /> */}
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/users" element={<UsersPage />}/>
+   
       </Routes>
     </div>
   );
